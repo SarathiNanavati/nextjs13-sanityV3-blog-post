9868647101ecb4be8f4e2d7ad0f91abb9849c28f
Sarathi Nanavati <sarathi.nanavati@gmail.com>
2022-12-16 14:32:55 +0530

    Upate title

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,13 +4,13 @@ const Banner = () => {
       className='flex flex-col justify-between font-bold px-10 py-5 mb-10
         lg:flex-row lg:space-x-5'>
       <div>
-        <h1 className='text-7xl'>Daily Blog Update</h1>
+        <h1 className='text-7xl'>Dev Blogoverse</h1>
         <h2
           className='mt-5
             md:mt-8'>
-          Welcome to{" "}
-          <span className='underline decoration-4 decoration-[#52daf2]'> Every Developers </span>{" "}
-          favourite blog in the Devosphere
+          Greetings to{" "}
+          <span className='underline decoration-4 decoration-[#52daf2]'> Every Developer</span>, Get
+          your favourite blogs on <span className='text-lg text-[#52daf2]'> Dev Blogoverser</span>
         </h2>
       </div>
       <p
