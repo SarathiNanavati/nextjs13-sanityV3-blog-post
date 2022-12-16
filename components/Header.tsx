4c03ca4f6e11f5352631b282d78c3e4599840982
Sarathi Nanavati <sarathi.nanavati@gmail.com>
2022-12-16 14:25:10 +0530

    Added bottom border on header and made it sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between space-x-2 font-bold px-10 py-5'>
+    <header
+      className='sticky top-0 flex items-center justify-between space-x-2 font-bold px-10 py-5 
+      border-b-4 border-[#52daf2]/50 backdrop-blur-3xl drop-shadow-md rounded z-10'>
       <div className='flex items-center space-x-2'>
         <Link href='/'>
           <Image className='rounded-full' src='/1.png' width={50} height={50} alt='' />
